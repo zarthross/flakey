ae111fc3a0ec3bfdf0cc069654da3c47c8922732
Darren Gibson <zarthross+github@gmail.com>
2022-09-01 17:30:44 -0500

    Fix updater

--- pkgs/bitwarden/update.sh
+++ pkgs/bitwarden/update.sh
@@ -7,7 +7,8 @@ basename="$(dirname $script)"
 
 
 function get_sha512() {    
-    curl -Ls $1 |  yq -r '.files | map(select(.url | test("\\.dmg$"))) | .[0].sha512'
+    shaFile=$(curl -Ls $1)
+    yq -r '.files | map(select(.url | test("\\.dmg$"))) | .[0].sha512' <<< "$shaFile"
 }
 
 function generate_json() {
@@ -15,7 +16,6 @@ function generate_json() {
 
     data=$(jq 'map(select(.tag_name | startswith("desktop-"))) | max_by(.tag_name) | { version: .tag_name | split("-v") | .[1], release_id: .id, } * (.assets | map(select(.name | test("\\.dmg$"))) | .[0] | {url: .browser_download_url, asset_id: .id, name: .name}) * (.assets | map(select(.name | test("^latest-mac.yml$"))) | .[0] | {sha_url: .browser_download_url, sha_asset_id: .id}) '\
       <<< "$releases")
-    
     
     sha=$(get_sha512 $(jq --jsonargs -r '.sha_url' <<< "$data"))
 
