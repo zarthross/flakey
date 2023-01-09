5857acfb112829df48c1da85e73a58fd4ae343e7
Darren Gibson <darren.gibson+github@banno.com>
2023-01-09 10:30:25 -0600

    Update bitwarden script

--- pkgs/bitwarden/update.sh
+++ pkgs/bitwarden/update.sh
@@ -14,7 +14,7 @@ function get_sha512() {
 function generate_json() {
     releases=$(gh api -H 'Accept: application/vnd.github+json' /repos/bitwarden/clients/releases)
 
-    data=$(jq 'map(select(.tag_name | startswith("desktop-"))) | max_by(.tag_name) | { version: .tag_name | split("-v") | .[1], release_id: .id, } * (.assets | map(select(.name | test("\\.dmg$"))) | .[0] | {url: .browser_download_url, asset_id: .id, name: .name}) * (.assets | map(select(.name | test("^latest-mac.yml$"))) | .[0] | {sha_url: .browser_download_url, sha_asset_id: .id}) '\
+    data=$(jq 'map(select(.tag_name | startswith("desktop-"))) | max_by(.tag_name | split("-v") | .[1] | split(".") | map(tonumber)) | { version: .tag_name | split("-v") | .[1], release_id: .id, } * (.assets | map(select(.name | test("\\.dmg$"))) | .[0] | {url: .browser_download_url, asset_id: .id, name: .name}) * (.assets | map(select(.name | test("^latest-mac.yml$"))) | .[0] | {sha_url: .browser_download_url, sha_asset_id: .id}) '\
       <<< "$releases")
     
     sha=$(get_sha512 $(jq --jsonargs -r '.sha_url' <<< "$data"))
