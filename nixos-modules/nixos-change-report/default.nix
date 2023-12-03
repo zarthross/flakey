300534f2894d254cf47d82febc777b95ed507ee5
Darren Gibson <zarthross@gmail.com>
2023-12-03 08:14:45 -0600

    Update change report for nixos to reflect changed activation script deps

--- nixos-modules/nixos-change-report/default.nix
+++ nixos-modules/nixos-change-report/default.nix
@@ -3,7 +3,7 @@
 {
   system.activationScripts = {
     nix-change-report = {
-      deps = [ "wrappers" "var" "binsh" "nix" "etc" "usrbinenv" "users" ];
+      deps = [ "binsh" "etc" "users" "usrbinenv" ];
       supportsDryActivation = true;
       text = ''
         PATH=$PATH:${pkgs.nix}/bin ${pkgs.nvd}/bin/nvd diff /run/current-system $systemConfig
