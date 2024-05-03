e9d538acdef97998a94fd24b34bcf5604f0feee6
Darren Gibson <zarthross@gmail.com>
2024-05-03 13:09:07 -0500

    rename and deprecate

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -1,7 +1,11 @@
 rec {
-  hm-change-report = ./hm-change-report;
+  nix-change-report = ./nix-change-report;
+  hm-change-report =
+    builtins.trace
+     "[1;31mwarning: hm-change-report is Deprecated, please use nix-change-report.["
+    nix-change-report;
   allow-unfree-predicates = ../nixos-modules/allow-unfree-predicates;
   default = {
-    imports = [ hm-change-report allow-unfree-predicates ];
+    imports = [ nix-change-report allow-unfree-predicates ];
   };
 }
