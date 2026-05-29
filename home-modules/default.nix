77332eefd069c5342794006d03cfe38063243907
Darren Gibson <zarthross+github@gmail.com>
2026-05-29 11:57:16 +0200

    Add ECA.

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -1,6 +1,7 @@
 rec {
   ghorg = ./ghorg;
   omniwm = ./omniwm;
+  eca = ./eca;
   nix-change-report = ./nix-change-report;
   hm-change-report = builtins.trace "[1;31mwarning: hm-change-report is Deprecated, please use nix-change-report.[" nix-change-report;
   allow-unfree-predicates = ../nixos-modules/allow-unfree-predicates;
