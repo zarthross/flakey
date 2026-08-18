9c4880143bff179e7bdb13c61e981325da11a0ab
Darren Gibson <zarthross+github@gmail.com>
2026-08-18 10:09:21 +0200

    remove unused file, fix 'isDarwin'

--- modules/bitwarden/bitwarden.nix
+++ modules/bitwarden/bitwarden.nix
@@ -3,7 +3,7 @@
   perSystem =
     { pkgs, lib, ... }:
     {
-      packages = lib.optionalAttrs pkgs.stdenv.isDarwin {
+      packages = lib.optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
         bitwarden = pkgs.callPackage ./bitwarden.pkg.nix { };
       };
     };
